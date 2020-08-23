18fe73cd11517a0eeda0f5e7521c758425717f87
Nguyễn Đỗ  Hoàng Đình <nguyendohoangdinh@MacBook-Pro-cua-Nguyen.local>
2020-08-23 12:30:21 +0700

    add slider show logo and contact icon

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -88,5 +88,28 @@ function nextImage() {
   // slideshowImages[currentImageCounter].style.display = "block";
   slideshowImages[currentImageCounter].style.opacity = 1;
 }
-        
+
+
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
 });
